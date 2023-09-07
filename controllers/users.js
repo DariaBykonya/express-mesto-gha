9a3c939d31a5d40fadfb0563e5fb41096435aa19
Daria Bykonya <dashabykonya@yandex.ru>
2023-09-07 22:17:18 +0200

    debug after test

--- controllers/users.js
+++ controllers/users.js
@@ -52,7 +52,7 @@ module.exports.createUser = (req, res, next) => {
       email,
       password: hash,
     })
-      .then((newUser) => {
+      .then(() => {
         res.status(201).send({
           data: {
             name, about, avatar, email,
